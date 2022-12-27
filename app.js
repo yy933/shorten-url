d40f57b59f687e9851dec3c19738ed49c3e8f941
yy933 <emilyyu025@gmail.com>
2022-12-27 17:03:34 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose') 
 
-const app = express()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const port = 3000
+const app = express();
+const port = 3000;
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URI) 
+
+const db = mongoose.connection;
+// 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+// 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 app.get('/', (req, res)=>{
   res.send('Success!')
