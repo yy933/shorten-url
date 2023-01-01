c6f7dd317ea5ca5bc11993623eb08f183d1a21ee
yy933 <emilyyu025@gmail.com>
2023-01-02 00:11:12 +0800

    feat: use JavaScript standard style to format code

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGODB_URI)
 
 const db = mongoose.connection
 // 連線異常
